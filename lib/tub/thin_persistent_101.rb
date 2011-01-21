403e95a9046694dcd3908a9709701f97f71d4275
Tim Shadel <github@timshadel.com>
2011-01-20 22:36:07 -0700

    Create a workaround for Thin's 101 handling
    Just until https://github.com/macournoyer/thin/pull/32 is taken care of.
    
    Signed-off-by: Tim Shadel <github@timshadel.com>

--- lib/tub/thin_persistent_101.rb
+++ lib/tub/thin_persistent_101.rb
@@ -21,12 +21,15 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #++
 
-module ThinUpgradableBackend
-  ROOT = File.expand_path(File.dirname(__FILE__))
-end
-TUB = ThinUpgradableBackend
+module Thin
 
-require "#{TUB::ROOT}/tub/version"
-require "#{TUB::ROOT}/tub/upgradable_connection"
-require "#{TUB::ROOT}/tub/upgradable_tcp_server"
-require "#{TUB::ROOT}/tub/thin_persistent_101"
+  # This is a workaround, and should be removed as soon as Heroku deploys a Thin version
+  # that includes some version of https://github.com/macournoyer/thin/pull/32
+  class Response
+    alias :old_persistent? :persistent?
+    def persistent?
+      old_persistent? || (@status == 100 || @status == 101)
+    end
+  end
+
+end
